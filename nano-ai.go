5a68602a31d8f8803c549de56059bc0f39a9ce72
janekjan <jas.piskorski@spoko.pl>
2014-10-21 22:22:10 +0200

    nadal jest błąd z input

--- nano-ai.go
+++ nano-ai.go
@@ -1,18 +1,30 @@
 /*MIT licence
 (c) Jan Piskorski, Bartosz Deptuła, Mateusz Boruń
- */
+*/
 
 package main
 
-import(
+import (
 	"fmt"
-	"strings"
+//	"strings"
+//	"bufio"
+//	"os"
 )
 
-func main(){
-	fmt.Println("NANO-AI 0.0.1")
-	
+func main() {
+	fmt.Println("nano 0.0.1")
+	rawinputted := GetQuery()
+	fmt.Printf("%v\n", rawinputted)
+
 }
 func GetQuery() string {
-	fmt.Scanf("")
+	var inp [100]string
+	var inp2 []string
+	
+	//reader := bufio.NewReader(os.Stdin)
+	//inp,_ := reader.ReadString('\n')
+	//strings.TrimSpace(inp)
+	fmt.Scanf("%s", &[]inp)
+	fmt.Println(inp)
+	return inp
 }
