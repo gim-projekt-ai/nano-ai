f7abf5d15654c55cade6b9181ca3eb35d719f24c
janekjan <jas.piskorski@spoko.pl>
2014-10-23 21:42:31 +0200

    nareszcie dziala scanf

--- nano-ai.go
+++ nano-ai.go
@@ -1,5 +1,6 @@
 /*MIT licence
 (c) Jan Piskorski, Bartosz Deptuła, Mateusz Boruń
+Wiktor Kacalski & Mikołaj Kordowski
 */
 
 package main
@@ -7,24 +8,20 @@ package main
 import (
 	"fmt"
 //	"strings"
-//	"bufio"
-//	"os"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println("nano 0.0.1")
-	rawinputted := GetQuery()
-	fmt.Printf("%v\n", rawinputted)
+	//fmt.Println("nano 0.0.1")
+	fmt.Println("Napisaleś: "+GetQuery())
 
 }
 func GetQuery() string {
-	var inp [100]string
-	var inp2 []string
-	
-	//reader := bufio.NewReader(os.Stdin)
-	//inp,_ := reader.ReadString('\n')
-	//strings.TrimSpace(inp)
-	fmt.Scanf("%s", &[]inp)
-	fmt.Println(inp)
+	var inp string
+	scnr := bufio.NewScanner(os.Stdin)
+	scnr.Scan()
+	inp = scnr.Text()
+	//fmt.Printf("%s\n", scnr.Text())
 	return inp
 }
