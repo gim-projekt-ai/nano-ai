5aca931456a62fdb6a3a447a766e626220402f78
janekjan <jas.piskorski@spoko.pl>
2014-10-24 19:02:22 +0200

    rozpoznawanie typu wypowiedzi

--- nano-ai.go
+++ nano-ai.go
@@ -6,21 +6,24 @@ Wiktor Kacalski & Mikołaj Kordowski
 package main
 
 import (
-	"fmt"
-//	"strings"
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("nano-ai 0.0.1")
+	//zmienne
 	var unprocQuery string
 	var purpose int8
+	//główna pętla
 	for {
 		unprocQuery = GetQuery()
 		purpose = Querypurpose(unprocQuery)
 		//typ qypowiedzi: 1 - inform.; 2- pyt. 3 - rozkaz
 		println(purpose, unprocQuery)
+		//na razie wychodzi
 		os.Exit(0)
 	}
 
@@ -34,5 +37,16 @@ func GetQuery() string {
 	return inp
 }
 func Querypurpose(query string) int8 {
-	return 1
+	var querytype int8 = 1
+	//jak zawiera *q to jest pytaniem
+	if strings.Contains(query, "*q") {
+		querytype = 2
+	}
+	//jak zawiera request to jest rozkazem
+	if strings.Contains(query, "*request") {
+		querytype = 3
+	}
+	//jeśliu nie zawiera żadnego z pow. to nadal =1
+	return querytype
+
 }
